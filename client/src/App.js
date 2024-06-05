3dc4b283dc40ab8dbe2c70fc20c8045a38096783
AYMAR ANTONIO DA SILVA <aymar.silva@labcetec.net>
2024-06-05 17:19:58 -0300

    Att

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,27 @@
-<<<<<<< HEAD
-import logo from './logo.svg'; 
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import CursosEmpresa from './views/CursosEmpresa';
-import Login from './views/Login';
-import NavBar from './views/navBar';
-import CursoInfo from './views/cursoInfo';
-=======
 import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CursosEmpresa from "./views/CursosEmpresa";
 import Login from './views/Login';
 import Fatura from "./views/Fatura";
 import Pagamento from './views/pagamento';
->>>>>>> 5c103a4548e1deca8b160c786e42ac945c146fcb
+import UploadCurso from './views/uploadCurso';
+import CursoInfo from './views/cursoInfo';
+import NavBar from './views/navBar';
 
 function App() {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
     <Routes>
       <Route path='/CursosEmpresa' element={<CursosEmpresa/>}/>
       <Route path='/login' element={<Login />}></Route>
       <Route path='/navBar' element={<NavBar />}></Route>
       <Route path='/cursoInfo' element={<CursoInfo />}></Route>
+      <Route path='/fatura' element={<Fatura />}></Route>
+      <Route path='/pagamento' element={<Pagamento />}></Route>
+      <Route path='/uploadCurso' element={<UploadCurso/>}></Route>
     </Routes>
-=======
-      <Routes>
-        <Route path='/cursosEmpresa' element={<CursosEmpresa />} />
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/fatura' element={<Fatura />} />
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/pagamento' element={<Pagamento />} />
-        <Route path='/login' element={<Login />}></Route>
-      </Routes>
->>>>>>> 5c103a4548e1deca8b160c786e42ac945c146fcb
     </BrowserRouter>
   );
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> 5c103a4548e1deca8b160c786e42ac945c146fcb

dec42922f6c644aaa61dd1991a0508598a8b4769
Marco Medeiros <marco.a.medeiros6@aluno.senai.br>
2024-09-24 02:32:14 -0300

    graficos atualizaçao

--- client/src/views/graficos/estatisticas.jsx
+++ client/src/views/graficos/estatisticas.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { Chart as ChartJS, defaults } from "chart.js/auto";
-import { Bar, Doughnut, Line } from "react-chartjs-2";
+import { Bar, Line } from "react-chartjs-2";
 import Cabecalho from "../cabecalho";
-
-import "./App.css";
-
+import "./App.css"; 
 import revenueData from "../../data/revenueData.json";
 import sourceData from "../../data/sourceData.json";
 
@@ -19,72 +17,74 @@ defaults.plugins.title.color = "black";
 export default function App() {
   return (
     <>
-    <Cabecalho/>
+      <Cabecalho />
+      <div className="App">
+        <div className="content-wrapper">
+          <div className="quadrado-perfil"></div>       
+            <div className="quadrado-grafico">
+              <div className="dataCard revenueCard">
+                <Line
+                  data={{
+                    labels: revenueData.map((data) => data.label),
+                    datasets: [
+                      {
+                        label: "Tempo",
+                        data: revenueData.map((data) => data.revenue),
+                        backgroundColor: "#064FF0",
+                        borderColor: "#064FF0",
+                      },
+                      {
+                        label: "Mês",
+                        data: revenueData.map((data) => data.cost),
+                        backgroundColor: "#FF3030",
+                        borderColor: "#FF3030",
+                      },
+                    ],
+                  }}
+                  options={{
+                    elements: {
+                      line: {
+                        tension: 0.5,
+                      },
+                    },
+                    plugins: {
+                      title: {
+                        text: "Tempo gasto na plataforma",
+                      },
+                    },
+                  }}
+                />
+              </div>
 
-    <div className="App">
-      <div className="grafico">
-      <div className="dataCard revenueCard">
-        <Line
-          data={{
-            labels: revenueData.map((data) => data.label),
-            datasets: [
-              {
-                label: "Tempo",
-                data: revenueData.map((data) => data.revenue),
-                backgroundColor: "#064FF0",
-                borderColor: "#064FF0",
-              },
-              {
-                label: "Mês",
-                data: revenueData.map((data) => data.cost),
-                backgroundColor: "#FF3030",
-                borderColor: "#FF3030",
-              },
-            ],
-          }}
-          options={{
-            elements: {
-              line: {
-                tension: 0.5,
-              },
-            },
-            plugins: {
-              title: {                
-                text: "Tempo gasto na plataforma",
-              },
-            },
-          }}
-        />
-      </div>
-          
-      <div className="dataCard customerCard">
-        <Bar
-          data={{
-            labels: sourceData.map((data) => data.label),
-            datasets: [
-              {
-                label: "Diárias",
-                data: sourceData.map((data) => data.value),
-                backgroundColor: [
-                  "rgba(43, 63, 229, 0.8)",
-                  "rgba(250, 192, 19, 0.8)",
-                  "rgba(253, 135, 135, 0.8)",
-                ],
-                borderRadius: 5,
-              },
-            ],
-          }}
-          options={{
-            plugins: {
-              title: {
-                text: "Check-ins diário",
-              },
-            },
-          }}
-        />
-      </div>      
-    </div>
-    </div>
-    </>    
+              <div className="dataCard customerCard">
+                <Bar
+                  data={{
+                    labels: sourceData.map((data) => data.label),
+                    datasets: [
+                      {
+                        label: "Diárias",
+                        data: sourceData.map((data) => data.value),
+                        backgroundColor: [
+                          "rgba(43, 63, 229, 0.8)",
+                          "rgba(250, 192, 19, 0.8)",
+                          "rgba(253, 135, 135, 0.8)",
+                        ],
+                        borderRadius: 5,
+                      },
+                    ],
+                  }}
+                  options={{
+                    plugins: {
+                      title: {
+                        text: "Check-ins diário",
+                      },
+                    },
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+    </>
   );
-};
+}

86c02c06f308b03dbe0e25e381aeac42557f024d
Marco Medeiros <marco.a.medeiros6@aluno.senai.br>
2024-10-17 20:22:23 -0300

    adicionando perfil e busca

--- client/src/views/graficos/estatisticas.jsx
+++ client/src/views/graficos/estatisticas.jsx
@@ -19,6 +19,7 @@ defaults.plugins.title.color = "black";
 
 export default function App() {
   const [showStats, setShowStats] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const toggleStats = () => {
     setShowStats(!showStats);
@@ -28,20 +29,91 @@ export default function App() {
     <>
       <Cabecalho />
       <div className="content-wrapper">
-        <section className="quadrado-perfil"></section>
+        <section className="quadrado-perfil">
+          <div style={{ position: 'relative' }}>
+            <input
+              type="text"
+              placeholder="Buscar colaboradores"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              style={{
+                width: '90%',
+                padding: '0.5em 0.8em',
+                borderRadius: '20px',
+                border: '1px solid #ccc',
+                outline: 'none',
+                margin: '1em 0',
+                transition: 'border-color 0.3s'
+              }}
+              onFocus={(e) => e.target.style.border = '1px solid #ccc'}
+              onBlur={(e) => e.target.style.border = '1px solid #ccc'}
+            />
+            <div style={{
+              position: 'absolute',
+              right: '80px',
+              top: '50%',
+              height: '20px',
+              width: '1px',
+              backgroundColor: '#999',
+              transform: 'translateY(-50%)'
+            }} />
+            <i className="fa fa-search"
+              style={{
+                position: 'absolute',
+                right: '40px',
+                top: '50%',
+                transform: 'translateY(-50%)',
+                color: '#999',
+                cursor: 'pointer'
+              }}
+            />
+          </div>
+          <div style={{
+            height: '1px',
+            backgroundColor: '#ccc',
+            margin: '0.5em 0',
+          }} />
+
+          <div className="perfil-section">
+            <img
+              src="http://localhost:3000/static/media/perfilEduardoTest.574b9d49835217977ba4.jpg"
+              alt="Perfil Eduardo"
+              style={{ width: '50px', height: '50px', borderRadius: '50%', marginRight: '1em', marginLeft: '0.5em', cursor: 'pointer', marginTop: '10px' }}
+            />
+            <span style={{ fontSize: '1.2em', color: 'black', fontWeight: 'bold', marginLeft: '0.5em', cursor: 'pointer', marginTop: '-10px' }}>Eduardo Pereira</span>
+          </div>
+          <div style={{
+            height: '1px',
+            backgroundColor: '#ccc',
+            margin: '1.5em auto',
+            width: '70%',
+          }} />
+
+          <div className="perfil-section">
+            <img
+              src="http://localhost:3000/static/media/perfilLeilaTest.8c8aa3515d4022ceb34d.jpg"
+              alt="Perfil Leila"
+              style={{ width: '50px', height: '50px', borderRadius: '50%', marginRight: '1em', marginLeft: '0.5em', cursor: 'pointer' }}
+            />
+            <span style={{ fontSize: '1.2em', color: 'black', fontWeight: 'bold', marginLeft: '0.5em', cursor: 'pointer', marginTop: '10px' }}>Leila Pereira</span>
+          </div>
+          <div style={{
+            height: '1px',
+            backgroundColor: '#ccc',
+            margin: '1.5em auto',
+            width: '70%',
+          }} />
+        </section>
+
         <section className="quadrado-grafico">
-
           <div className="perf-colab">
-
             <div className="foto-colab">
-              <img src={ imgs.tabEmpty } alt="colab-foto" />
+              <img src={imgs.tabEmpty} alt="colab-foto" />
             </div>
 
-            {/* Filtrar por materia do colaborador */}
             <div className="mat-filtro">
-
               <div className="materia-filtra">
-                <img src={ imgs.filter } alt="icon-filtro" />
+                <img src={imgs.filter} alt="icon-filtro" />
                 <ul>
                   <li>Materia 1</li>
                   <li>Materia 2</li>
@@ -53,78 +125,69 @@ export default function App() {
                 <h2>Best Score</h2>
                 <p className="circle">Pontuacao</p>
               </div>
-
             </div>
-
           </div>
-          {/* <button onClick={toggleStats} className="estatisticas-button">
-            Estatísticas <span className={showStats ? "fa fa-chevron-up" : "fa fa-chevron-down"}></span>
-          </button>
 
-          {showStats && (
-            <> */}
-              <div className="dataCard revenueCard">
-                <Line
-                  data={{
-                    labels: revenueData.map((data) => data.label),
-                    datasets: [
-                      {
-                        label: "Tempo",
-                        data: revenueData.map((data) => data.revenue),
-                        backgroundColor: "#064FF0",
-                        borderColor: "#064FF0",
-                      },
-                      {
-                        label: "Mês",
-                        data: revenueData.map((data) => data.cost),
-                        backgroundColor: "#FF3030",
-                        borderColor: "#FF3030",
-                      },
-                    ],
-                  }}
-                  options={{
-                    elements: {
-                      line: {
-                        tension: 0.5,
-                      },
-                    },
-                    plugins: {
-                      title: {
-                        text: "Tempo gasto na plataforma",
-                      },
-                    },
-                  }}
-                />
-              </div>
+          <div className="dataCard revenueCard">
+            <Line
+              data={{
+                labels: revenueData.map((data) => data.label),
+                datasets: [
+                  {
+                    label: "Tempo",
+                    data: revenueData.map((data) => data.revenue),
+                    backgroundColor: "#064FF0",
+                    borderColor: "#064FF0",
+                  },
+                  {
+                    label: "Mês",
+                    data: revenueData.map((data) => data.cost),
+                    backgroundColor: "#FF3030",
+                    borderColor: "#FF3030",
+                  },
+                ],
+              }}
+              options={{
+                elements: {
+                  line: {
+                    tension: 0.5,
+                  },
+                },
+                plugins: {
+                  title: {
+                    text: "Tempo gasto na plataforma",
+                  },
+                },
+              }}
+            />
+          </div>
 
-              <div className="dataCard customerCard">
-                <Bar
-                  data={{
-                    labels: sourceData.map((data) => data.label),
-                    datasets: [
-                      {
-                        label: "Diárias",
-                        data: sourceData.map((data) => data.value),
-                        backgroundColor: [
-                          "rgba(43, 63, 229, 0.8)",
-                          "rgba(250, 192, 19, 0.8)",
-                          "rgba(253, 135, 135, 0.8)",
-                        ],
-                        borderRadius: 5,
-                      },
+          <div className="dataCard customerCard">
+            <Bar
+              data={{
+                labels: sourceData.map((data) => data.label),
+                datasets: [
+                  {
+                    label: "Diárias",
+                    data: sourceData.map((data) => data.value),
+                    backgroundColor: [
+                      "rgba(43, 63, 229, 0.8)",
+                      "rgba(250, 192, 19, 0.8)",
+                      "rgba(253, 135, 135, 0.8)",
                     ],
-                  }}
-                  options={{
-                    plugins: {
-                      title: {
-                        text: "Check-ins diário",
-                      },
-                    },
-                  }}
-                />
-              </div>
-            {/* </>
-          )} */}
+                    borderRadius: 5,
+                  },
+                ],
+              }}
+              options={{
+                plugins: {
+                  title: {
+                    text: "Check-ins diário",
+                  },
+                },
+              }}
+            />
+          </div>
         </section>
       </div>
     </>

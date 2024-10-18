5b4250c07afc11059be3fb9b53f9903667b75716
Marco Medeiros <marco.a.medeiros6@aluno.senai.br>
2024-10-17 22:47:23 -0300

    atualizando a lading page

--- client/src/views/lading-page/lading.jsx
+++ client/src/views/lading-page/lading.jsx
@@ -1,20 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import imgs from '../../imgs/arrayImagens';
 import '../lading-page/ladingEstilo.css';
 
 const App = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const services = [
+    { img: imgs.treino1, title: 'Equipamento de proteção individual' },
+    { img: imgs.treino2, title: 'Treinamento de Prevenção de Incêndios' },
+    { img: imgs.treino3, title: 'Processamento de extrato de Frutas' },
+    { img: imgs.treino4, title: 'Treinamento Primeiro Socorros' },
+    { img: imgs.treino5, title: 'Processo de congelamento de Frutas' },
+  ];
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % services.length);
+    setIsHovered(false);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + services.length) % services.length);
+    setIsHovered(false);
+  };
+
+  const goToSlide = (index) => {
+    setCurrentIndex(index);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!isHovered) {
+        nextSlide();
+      }
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [isHovered]);
+
   return (
-    <div>
+    <div className="inicial">
       <div className="navegacao">
         <div className="menu-area">
-          <img src={imgs.TreinOffer} alt="logo" />
-          <div className="menu">
-            <a href="#">INICIO</a>
-            <a href="#">QUEM SOMOS</a>
-            <a href="#">SERVIÇOS</a>
-            <a href="#">CONTATO</a>
-            <a href="#">JÁ TEM UMA CONTA?</a>
-          </div>
+          <a href="/" rel="noopener noreferrer">
+            <img
+              src={imgs.TreinOffer}
+              alt="Logo do TreinOffer"
+              className="logo-treino"
+              style={{ width: '300px', height: 'auto' }}
+            />
+          </a>
+          <nav className="menu">
+            <a href="/">Início</a>
+            <a href="#">Quem Somos</a>
+            <a href="#">Serviços</a>
+            <a href="#">Contato</a>
+            <div className="dropdown">
+              <button className="dropdown-toggle">Já tem uma conta?</button>
+              <div className="dropdown-content">
+                <a href="/login">
+                  <i className="fa fa-sign-in" aria-hidden="true"></i> Logar
+                </a>
+                <a href="/login">
+                  <i className="fa fa-user-plus" aria-hidden="true"></i> Cadastrar
+                </a>
+              </div>
+            </div>
+          </nav>
         </div>
         <hr className="hr-separador" />
       </div>
@@ -38,58 +90,62 @@ const App = () => {
       <div className="servicos">
         <div className="bloco-set">
           <div className="servicos">
-            <b>Serviços</b>
+            <h2 style={{ fontWeight: 'bold' }}>Serviços</h2>
           </div>
-          <div className="grid-container">
-            <div className="grid-item">
-              <img src={imgs.treino1} alt="" />
-              <h1 className="titulo-principal">Equipamento de proteção individual</h1>
+          <div className="carousel">
+            <button onClick={prevSlide} className="carousel-button">❮</button>
+            <div className="carousel-item">
+              <div className="image-container">
+                <img
+                  src={services[currentIndex].img}
+                  alt={services[currentIndex].title}
+                  className={isHovered ? '' : 'blurred'}
+                  onMouseEnter={() => setIsHovered(true)}
+                  onMouseLeave={() => setIsHovered(false)}
+                />
+                {isHovered && <h1 className="titulo-principal">{services[currentIndex].title}</h1>}
+              </div>
             </div>
-            <div className="grid-item">
-              <img src={imgs.treino2} alt="" />
-              <h1 className="titulo-principal">Treinamento de Prevenção de Incêndios</h1>
-            </div>
-            <div className="grid-item">
-              <img src={imgs.treino3} alt="" />
-              <h1 className="titulo-principal">Processamento de extrato de Frutas</h1>
-            </div>
-            <div className="grid-item">
-              <img src={imgs.treino4} alt="" />
-              <h1 className="titulo-principal">Treinamento Primeiro Socorros</h1>
-            </div>
-            <div className="grid-item">
-              <img src={imgs.treino5} alt="" />
-              <h1 className="titulo-principal">Processo de congelamento de Frutas</h1>
-            </div>
+            <button onClick={nextSlide} className="carousel-button">❯</button>
+          </div>
+          <div className="indicators">
+            {services.map((_, index) => (
+              <span
+                key={index}
+                className={`indicator ${currentIndex === index ? 'active' : ''}`}
+                onClick={() => goToSlide(index)}
+              />
+            ))}
           </div>
         </div>
       </div>
       <div className="contato">
-        <div className="texto-colorido">
-          <b>Contato</b>
-        </div>
-        <div className="campos">
-          <label htmlFor="nome">Digite seu nome inteiro:</label>
-          <input className="input-texto" type="text" name="nome" id="nome" />
-        </div>
-        <div className="campos">
-          <label htmlFor="telef">Digite seu número de telefone:</label>
-          <input className="input-telefone" type="tel" name="telef" id="telef" placeholder="Ex: 00999999999" />
-        </div>
-        <div className="campos">
-          <label htmlFor="email">Digite seu email:</label>
-          <input className="input-email" type="email" name="email" id="email" placeholder="Ex: exemplo@mail.com" />
-        </div>
-        <div className="campos">
-          <label htmlFor="gen">Qual seu gênero:</label>
-          <select className="select" id="gen" name="gen">
-            <option value="" hidden selected></option>
-            <option value="masculino">Masculino</option>
-            <option value="feminino">Feminino</option>
-            <option value="neutro">Neutro</option>
-            <option value="outro">Prefiro não informar</option>
-          </select>
-        </div>
+        <h2 className="titulo-contato">Contato</h2>
+        <form className="formulario">
+          <div className="campo">
+            <label htmlFor="nome">Nome completo:</label>
+            <input className="input-texto" type="text" name="nome" id="nome" required />
+          </div>
+          <div className="campo">
+            <label htmlFor="telef">Número de telefone:</label>
+            <input className="input-telefone" type="tel" name="telef" id="telef" placeholder="Ex: 00999999999" required />
+          </div>
+          <div className="campo">
+            <label htmlFor="email">E-mail:</label>
+            <input className="input-email" type="email" name="email" id="email" placeholder="Ex: exemplo@mail.com" required />
+          </div>
+          <div className="campo">
+            <label htmlFor="gen">Gênero:</label>
+            <select className="select" id="gen" name="gen">
+              <option value="" hidden>Selecione</option>
+              <option value="masculino">Masculino</option>
+              <option value="feminino">Feminino</option>
+              <option value="neutro">Neutro</option>
+              <option value="outro">Prefiro não informar</option>
+            </select>
+          </div>
+          <button type="submit" className="botao-enviar">Enviar</button>
+        </form>
       </div>
     </div>
   );

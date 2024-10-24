1b137c7d8baaf7ea7f0935e0331695698ff3e150
Marco Medeiros <marco.a.medeiros6@aluno.senai.br>
2024-10-23 22:20:04 -0300

    atuualizando cadastro

--- client/src/views/cadastro/cadastrar.jsx
+++ client/src/views/cadastro/cadastrar.jsx
@@ -21,6 +21,10 @@ function Cadastro() {
                                     <td><label htmlFor="cnpj">CNPJ:</label></td>
                                     <td><input placeholder='Inserir CNPJ' type="text" name="cnpj" id="cnpj" /></td>
                                 </tr>
+                                <tr className="campo_nome_empresa">
+                                    <td><label htmlFor="nome_empresa">Nome da Empresa:</label></td>
+                                    <td><input placeholder='Inserir Nome da Empresa' type="text" name="nome_empresa" id="nome_empresa" /></td>
+                                </tr>
                                 <tr className="campo_senha">
                                     <td><label htmlFor="senha">Senha:</label></td>
                                     <td><input placeholder='Inserir senha' type="password" name="senha" id="senha" /></td>
@@ -28,6 +32,41 @@ function Cadastro() {
                                 <tr className="campo_cep">
                                     <td><label htmlFor="cep">CEP:</label></td>
                                     <td><input placeholder='Inserir CEP' type="text" name="cep" id="cep" /></td>
+                                </tr>
+                                <tr className="campo_estado">
+                                    <td><label htmlFor="estados" id='estados'>Estado:</label></td>
+                                    <td>
+                                        <select name="estado" id="estado">
+                                            <option value="" id='select'>Selecionar Estado</option>
+                                            <option value="AC">Acre</option>
+                                            <option value="AL">Alagoas</option>
+                                            <option value="AP">Amapá</option>
+                                            <option value="AM">Amazonas</option>
+                                            <option value="BA">Bahia</option>
+                                            <option value="CE">Ceará</option>
+                                            <option value="DF">Distrito Federal</option>
+                                            <option value="ES">Espírito Santo</option>
+                                            <option value="GO">Goiás</option>
+                                            <option value="MA">Maranhão</option>
+                                            <option value="MT">Mato Grosso</option>
+                                            <option value="MS">Mato Grosso do Sul</option>
+                                            <option value="MG">Minas Gerais</option>
+                                            <option value="PA">Pará</option>
+                                            <option value="PB">Paraíba</option>
+                                            <option value="PR">Paraná</option>
+                                            <option value="PE">Pernambuco</option>
+                                            <option value="PI">Piauí</option>
+                                            <option value="RJ">Rio de Janeiro</option>
+                                            <option value="RN">Rio Grande do Norte</option>
+                                            <option value="RS">Rio Grande do Sul</option>
+                                            <option value="RO">Rondônia</option>
+                                            <option value="RR">Roraima</option>
+                                            <option value="SC">Santa Catarina</option>
+                                            <option value="SP">São Paulo</option>
+                                            <option value="SE">Sergipe</option>
+                                            <option value="TO">Tocantins</option>
+                                        </select>
+                                    </td>
                                 </tr>
                                 <tr className="campo_telefone">
                                     <td><label htmlFor="telefone">Telefone:</label></td>
@@ -37,6 +76,10 @@ function Cadastro() {
                                     <td><label htmlFor="endereco">Endereço:</label></td>
                                     <td><input placeholder='Inserir Endereço' type="text" name="endereco" id="endereco" /></td>
                                 </tr>
+                                <tr className="campo_cidade">
+                                    <td><label htmlFor="cidade">Cidade:</label></td>
+                                    <td><input placeholder='Inserir Cidade' type="text" name="cidade" id="cidade" /></td>
+                                </tr>                                
                                 <tr>
                                     <td><input className='campo_submit' type="submit" value="Cadastrar" /></td>
                                 </tr>
@@ -56,7 +99,7 @@ function Cadastro() {
                 <section className="secao_logo">
                     <a href="/">
                         <img id="logo" src={imgs.TreinOffer} alt="Logo" />
-                    </a>                    
+                    </a>
                 </section>
             </main>
         </>

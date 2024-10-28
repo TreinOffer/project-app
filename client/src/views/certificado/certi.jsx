ed25ae8353566fe47eb7bf4f9c8b5911d51aa9b3
HELOISA RANGEL GUIMARÃES <h.guimaraes@labcetec.net>
2024-10-28 17:24:01 -0300

    certi

--- client/src/views/certificado/certi.jsx
+++ client/src/views/certificado/certi.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import Cabecalho from '../cabecalho';
 import './estCerti.css';
+
+
+
 
 function Certificado() {
   const handlePrint = () => {
@@ -9,17 +11,16 @@ function Certificado() {
 
   return (
     <>
-      <Cabecalho />
-      <div className="certificado-container">
+      <div className="certificado-container">,
         <div className="certificado">
-          <h1>Certificado de Conclusão</h1>
+          <h1>Certificado</h1>
           <p className="subtitulo">Este certificado é concedido a:</p>
           <h2 className="nome-participante">Nome do Participante</h2>
           <p className="subtitulo">Por ter concluído com sucesso o curso:</p>
           <h3 className="titulo-curso">Título do Curso</h3>
           <p className="data">Data: <strong>DD/MM/AAAA</strong></p>
           <div className="assinaturas">
-            <p>____________________</p>
+            <p>________________________</p>
             <p>Nome do Instrutor</p>
             <p>Assinatura</p>
           </div>

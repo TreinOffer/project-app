14472a6fb2725be6b94b7184d29c27c0e00eea39
Marco Medeiros <marco.a.medeiros6@aluno.senai.br>
2024-11-12 21:17:06 -0300

    att cod

--- client/src/views/tela-treino/telaModule.jsx
+++ client/src/views/tela-treino/telaModule.jsx
@@ -5,19 +5,7 @@ import Cabecalho from '../cabecalho/cabecalho';
 function TelaModule() {        
   return (
     <>
-      <Cabecalho /> 
-      <div className="book-container">
-        
-      <div className="book">
-        <div className="book-cover front"></div>
-        <div className="book-cover back"></div>
-        <div className="book-pages">
-          <div className="page"></div>
-          <div className="page"></div>
-          <div className="page"></div>
-        </div>
-      </div>
-    </div>
+      <Cabecalho />       
     </>
   );  
 }

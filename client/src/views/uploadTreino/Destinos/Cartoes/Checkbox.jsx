803f3c9743bb24ab39086ebcd8bb1478df964ed8
Marco Medeiros <marco.a.medeiros6@aluno.senai.br>
2024-12-11 02:23:34 -0300

    colocando checkbox pra funcionar

--- client/src/views/uploadTreino/Destinos/Cartoes/Checkbox.jsx
+++ client/src/views/uploadTreino/Destinos/Cartoes/Checkbox.jsx
@@ -1,66 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import './Checkbox.css';
 
-const NovoCheckbox = ({ index, mensagem, deletar, updateTexto, isEditing, setItens, modulos }) => {
-    const [texto, setTexto] = useState(mensagem);  
+const Checkbox = ({ index, mensagem, deletar, updateTexto, isEditing, setItens, modulos }) => {
+    const [texto, setTexto] = useState(mensagem);
     const [checked, setChecked] = useState(false);  
+    const [isEditingText, setIsEditingText] = useState(false); 
 
     useEffect(() => {
         setTexto(mensagem); 
     }, [mensagem]);
 
-    const toggleEdit = () => {
-
-        if (typeof updateTexto === 'function') {
-            updateTexto(index, texto);  
-        }
-
+    function actionEdit() {
         if (setItens && typeof setItens[1] === 'function') {
             setItens[1](prevItens => {
-                const itens = [...prevItens];
+                const itens = [...prevItens]; 
+
                 if (itens[modulos]) {
-                    itens[modulos] = itens[modulos].map((item, i) =>
-                        i === index ? { ...item, src: texto, checked, isOpen: false } : item 
-                    );
+                    itens[modulos] = itens[modulos].map((item, i) => {
+                        if (i === index) {
+                            return { 
+                                ...item,  
+                                isOpen: !item.isOpen, 
+                                src: texto, 
+                                checked: checked 
+                            };
+                        }
+                        return item;
+                    });
                 }
+
                 return itens;
             });
         } else {
             console.error("setItens não foi passado corretamente para o componente!");
         }
-    };
+        setIsEditingText(false);
+    }
 
     const onChange = (e) => {
-        setTexto(e.target.value); 
+        setTexto(e.target.value);
     };
 
     const handleCheckboxChange = () => {
-        setChecked(!checked);  
+        setChecked(prevChecked => !prevChecked);
     };
 
     return (
         <div id={`checkboxItem${index}`} className='novo-checkbox'>
-            <input
-                type="checkbox"
-                checked={checked}
-                onChange={handleCheckboxChange}
-                className="checkbox-input"
-            />
-            
-            {isEditing ? (
-                <p onClick={() => setItens && setItens[1](prevItens => {
-                    const itens = [...prevItens];
-                    if (itens[modulos]) {
-                        itens[modulos] = itens[modulos].map((item, i) =>
-                            i === index ? { ...item, isOpen: !item.isOpen } : item
-                        );
-                    }
-                    return itens;
-                })} className="texto-parag">
-                    {texto}
-                </p>
-            ) : (
-                <>
+            <div className="checkbox-container">
+                <input
+                    type="checkbox"
+                    checked={checked} 
+                    onChange={handleCheckboxChange}  
+                    className="checkbox-input"
+                />
+                
+                {isEditingText ? (
                     <textarea
                         className="textarea-edit"
                         autoCapitalize='on'
@@ -69,14 +64,21 @@ const NovoCheckbox = ({ index, mensagem, deletar, updateTexto, isEditing, setIte
                         onChange={onChange}
                         value={texto} 
                     />
-                    <div className='actions-checkbox'>
-                        <button onClick={toggleEdit} className='button-ok' type="button">OK</button>
-                        <button onClick={() => deletar(index)} className='button-remove' type="button">Excluir</button>
-                    </div>
-                </>
-            )}
+                ) : (
+                    <p onClick={() => setIsEditingText(true)} className="texto-parag">
+                        {texto}
+                    </p>
+                )}
+            </div>
+
+            <div className='actions-checkbox'>
+                {isEditingText && (
+                    <button onClick={actionEdit} className='button-ok' type="button">OK</button>
+                )}
+                <button onClick={() => deletar(index)} className='button-remove' type="button">Excluir</button>
+            </div>
         </div>
     );
 };
 
-export default NovoCheckbox;
+export default Checkbox;
